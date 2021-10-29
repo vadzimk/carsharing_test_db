4a53b14495473e2005eb94ae58289b0d27a75a61
Vadzim <vadzimk@hotmail.com>
2021-10-28 20:56:37 -0700

    change database name during initialization

--- postgres-init-db.sh
+++ postgres-init-db.sh
@@ -2,4 +2,4 @@
 set -e
 
 createdb -U postgres -T template0 car_sharing_container_test
-pg_restore --dbname=car_sharing_container --verbose ../backup/dump.tar
+pg_restore --dbname=car_sharing_container_test --verbose ../backup/dump.tar
